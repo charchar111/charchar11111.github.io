0f19c02974cb526079685fb8a4b28432b101ef8a
charchar111 <114625601+charchar111@users.noreply.github.com>
2022-11-07 05:20:26 +0900

    fixed:img bug

--- js/backgroundimage.js
+++ js/backgroundimage.js
@@ -3,7 +3,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 bgImage.className = "background-image";
 bgImage.src = `image/${chosenImage}`;
-bgImage.src = `file:///C:/Users/3hjab/Desktop/momentum-clone/img/${chosenImage}`;
+bgImage.src = `./img/${chosenImage}`;
 console.log(bgImage);
 
 document.body.appendChild(bgImage);
