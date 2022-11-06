a5fb99a5a4f11fecd830b0037076f30bd10fb18b
charchar111 <114625601+charchar111@users.noreply.github.com>
2022-11-07 02:23:14 +0900

    basic 2

--- js/backgroundimage.js
+++ js/backgroundimage.js
@@ -1,8 +1,9 @@
-const images = ["0.JPG", "1.JPG", "2.JPG", "3.JPG"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
-
+bgImage.className = "background-image";
 bgImage.src = `image/${chosenImage}`;
+bgImage.src = `file:///C:/Users/3hjab/Desktop/momentum-clone/img/${chosenImage}`;
 console.log(bgImage);
 
 document.body.appendChild(bgImage);
